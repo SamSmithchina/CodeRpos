6c6608142e9edeb7740454cdabf4b9b25b89141a
SamSmithchina <2998188865@qq.com>
2018-11-19 16:35:06 +0800

    2018-11-19

---
+++ Rpos/if(0).cpp
@@ -0,0 +1,24 @@
+#include <iostream> 
+
+using namespace std;
+
+int main()
+{
+	if(0)
+	{
+		cout<< "if(0) 为真 ！" << endl; 
+	}
+	else
+	{
+		cout<< "if(0) 为假 ！" << endl; 
+	}
+	return 0;
+}
+/*
+if(0) 为假 ！
+
+--------------------------------
+Process exited after 0.3746 seconds with return value 0
+请按任意键继续. . .
+*/ 
+ 
